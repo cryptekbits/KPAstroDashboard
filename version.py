9de94c38d366cb844acb141cac722db81eb4b15f
cryptekbits <ramnani.manan@gmail.com>
2025-03-02 21:43:27 +0530

    chore: Update version and gitignore
    - Bumped version to 1.0.1 with "BugFix Release" label
    - Added config.json and *.zip to .gitignore to prevent accidental tracking

--- version.py
+++ version.py
@@ -4,8 +4,8 @@ This file is updated with each release.
 """
 
 # Version information
-VERSION = "1.0.0"
-VERSION_NAME = "Initial Release"
+VERSION = "1.0.1"
+VERSION_NAME = "BugFix Release"
 BUILD_DATE = "2025-03-02"
 
 # GitHub repository information
