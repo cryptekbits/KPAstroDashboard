eb164c169b9ce097ee4fb84cc5eca19276f8c3f0
cryptekbits <ramnani.manan@gmail.com>
2025-03-03 03:12:18 +0530

    chore: Bump version to 1.0.4 with updated build date

--- version.py
+++ version.py
@@ -4,9 +4,9 @@ This file is updated with each release.
 """
 
 # Version information
-VERSION = "1.0.3"
-VERSION_NAME = "Alpha Release"
-BUILD_DATE = "2025-06-13"
+VERSION = "1.0.4"
+VERSION_NAME = "Alpha Release (Stable)"
+BUILD_DATE = "2025-03-03"
 
 # GitHub repository information
 GITHUB_REPO_OWNER = "cryptekbits"  # Replace with actual GitHub username
