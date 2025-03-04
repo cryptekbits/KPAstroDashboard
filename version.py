6cf1b62800bc80a387f9a89ce14a64466200baca
cryptekbits <ramnani.manan@gmail.com>
2025-03-04 14:34:55 +0530

    Release v1.1.1

--- version.py
+++ version.py
@@ -4,8 +4,8 @@ This file is updated with each release.
 """
 
 # Version information
-VERSION = "1.1.0"
-VERSION_NAME = "AI 1.1.0 - Alpha"
+VERSION = "1.1.1"
+VERSION_NAME = "AI 1.1.1 - Alpha"
 BUILD_DATE = "2025-03-04"
 
 # GitHub repository information
