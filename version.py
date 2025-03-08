4af084ab685db4b0a31773679b862c6ce9c31785
cryptekbits <ramnani.manan@gmail.com>
2025-03-08 15:30:53 +0530

    Release v1.5.3

--- version.py
+++ version.py
@@ -6,8 +6,8 @@ import os
 from datetime import datetime
 
 # Default version information
-VERSION = "1.5.2"
-VERSION_NAME = "AstroInsight-1.5.2-BugFix"
+VERSION = "1.5.3"
+VERSION_NAME = "AstroInsight-1.5.3-BugFix"
 BUILD_DATE = "2025-03-08"
 
 # Check for environment variables that might override the version
